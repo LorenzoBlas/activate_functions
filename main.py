dc665dbf00b19970a3f49d9ca695b2ff1811f807
Gamitasvp <LorenzoBlas>
2025-03-07 11:46:23 -0600

    Comit777v3

--- main.py
+++ main.py
@@ -7,26 +7,18 @@ def main():
     sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'srcs'))
 
     # Importar y ejecutar las funciones de activación
-<<<<<<< HEAD
     from activation_functions.relu import relu
     from activation_functions.sigmoid import sigmoid
     from activation_functions.tanh import tanh
     from activation_functions.leaky_relu import leaky_relu
-=======
-    from activation_functions.relu import plot_relu
-    from activation_functions.sigmoid import plot_sigmoid
-    from activation_functions.tanh import plot_tanh
-    from activation_functions.leaky_relu import plot_leaky_relu
->>>>>>> 4eab29cb83afb3394287c046df848f55a3bd7db1
 
-    funciones = [plot_relu, plot_sigmoid, plot_tanh, plot_leaky_relu]
+    funciones = [relu, sigmoid, tanh, leaky_relu]
     for func in funciones:
         try:
             func()
-            plt.show()
+            plt.show() 
         except Exception as e:
-            print(f"Error al mostrar la gráfica: {e}")
+            print(f"wao") 
 
 if __name__ == "__main__":
     main()
-
